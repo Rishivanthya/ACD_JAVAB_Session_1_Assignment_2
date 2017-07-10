6c9efca01ff22c87a22b9576a9735a3920bf096f
Rishivanthya <Rishivanthya@LAPTOP-93S6EELP>
2017-07-10 23:01:07 +0530

    second commit

---
+++ src/Addtion.java
@@ -0,0 +1,12 @@
+
+public class Addtion {
+	
+	  public static void main(String[] args)
+	  {
+		  int x=10;
+		  int y=12;
+		  int c=0;
+		  c= x & y;
+		  System.out.println("sum of two numbers without + operators :" + c);
+	  }
+}
